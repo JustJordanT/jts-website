7749a6bd58162d77e98f8c908a755862958c2f4f
Jordan <Jordantay9014@outlook.com>
2023-09-16 00:39:46 -0500

    feat - fixes

---
+++ components/projectcard_compoment.jsx
@@ -0,0 +1,17 @@
+// ProjectCard.js
+import React from 'react';
+
+const ProjectCard = ({ title, description, imageSrc, projectUrl }) => {
+  return (
+    <div className="project-card">
+      <img src={imageSrc} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <a href={projectUrl} target="_blank" rel="noopener noreferrer">
+        View on GitHub
+      </a>
+    </div>
+  );
+};
+
+export default ProjectCard;
